4108b36b72800edff59dffb5867bc19da0201f99
nuggy875 <jinugpaeng@gmail.com>
2018-10-21 22:34:07 +0900

    ref. ETRI 음성분석코드

--- src/api/hovi/answer.js
+++ src/api/hovi/answer.js
@@ -1,8 +1,41 @@
-
 async function getAnswer(req, res) {
   const { question } = req.query || {};
+  const { questionAnalyzed } = {};
+
+  const answer = {
+    category: 'cate',
+    question: question,
+    response: 'res'
+  }
+
+  // --- referenced from ETRI --
+  var openApiURL = 'http://aiopen.etri.re.kr:8000/WiseNLU';
+  // access 키 받으면 넣으면 됨
+  var access_key = 'YOUR_ACCESS_KEY';
+  var analysisCode = 'ANALYSIS_CODE';
+  
+  var requestJson = {
+      'access_key': access_key,
+      'argument': {
+          'text': question,
+          'analysis_code': analysisCode
+      }
+  };
+  
+  var request = require('request');
+  var options = {
+      url: openApiURL,
+      body: JSON.stringify(requestJson),
+      headers: {'Content-Type':'application/json; charset=UTF-8'}
+  };
+  request.post(options, function (error, response, body) {
+      console.log('responseCode = ' + response.statusCode);
+      console.log('responseBody = ' + body);
+  });
+
   try {
-    res.status(200).send(`success: your question is ${question}`);
+    //res.status(200).send(`success: your question is ${question}`);
+    res.status(200).json(answer);
   } catch (err) {
     res.status(500).send('fail');
   }
--- src/api/router.js
+++ src/api/router.js
@@ -7,4 +7,5 @@ const hovi = require('./hovi/router');
 const router = express.Router();
 
 router.use('/hovi', hovi);
+
 module.exports = router;
