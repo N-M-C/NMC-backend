52b3171cd27a1460bd22ccb17fb526c62b624bcb
seungkyu2 <zx6658@naver.com>
2018-10-24 23:17:34 +0900

    feat: question purify 해주는 부분 변수로 뺌

--- src/api/hovi/answer.js
+++ src/api/hovi/answer.js
@@ -16,6 +16,8 @@ async function getAnswer(req, res) {
     questionAnalyzed
   } = {};
 
+  const purifiedQuestion = question.replace(/ /gi, '');
+
   // 일단 dummy 데이터
   const answer = {
     question: question,
@@ -99,21 +101,21 @@ async function getAnswer(req, res) {
         // X-Ray, CT, MRI 등을 음성인식이 어떻게 인식하는지 봐야할 듯.
         // 위치같은 경우는 지도도 같이 띄워주면 좋을듯
         case 2:
-          if(question.replace(/ /gi, '').indexOf('편의시설')!=-1){
+          if(purifiedQuestion.indexOf('편의시설')!=-1){
             console.log('본관인 경우 – 지하1층에 아티제, 우체국, 선물의 집, 신한은행, 안경점, 의료용품점, 이/미용실, 편의점, 식당, 등이 있습니다.\n암병원인 경우 – 지하1층에 선물의 집, 신한은행, 커피전문점, 편의점, 식당 등이 있습니다.');
-          }else if(question.replace(/ /gi, '').indexOf('원무')!=-1){
+          }else if(purifiedQuestion.indexOf('원무')!=-1){
             console.log('환자분들의 편의를 위하여 원무창구를 병원업계최초로 운영하고 있습니다.\n병동 내 1:1 전담 직원 상주로 입/퇴원수속, 중간 진료비 수납, 진료비 상담, 제증명 발급, 퇴원 후 외래 예약, 기타 제안 및 문의사항 가능합니다.\n위치는 각 병동 원무 창구에 있고 이용시간은 평일 08:30~17:00입니다.\n이외의 시간에는 1층 원무창구를 이용해주십시오.');
-          }else if(question.replace(/ /gi, '').indexOf('엑스레이검사실')!=-1){
+          }else if(purifiedQuestion.indexOf('엑스레이검사실')!=-1){
             console.log('엑스레이실은 어디 있나요?');
-          }else if(question.replace(/ /gi, '').indexOf('CT검사실')!=-1){
+          }else if(purifiedQuestion.indexOf('CT검사실')!=-1){
             console.log('CT 검사실은 본관 1층 응급실 맞은편에 있습니다.');
-          }else if (question.replace(/ /gi, '').indexOf('MRI검사실')!=-1){
+          }else if (purifiedQuestion.indexOf('MRI검사실')!=-1){
             console.log('MRI 검사실은 본관 1층 응급실 맞은편에 있습니다.');
-          }else if (question.replace(/ /gi, '').indexOf('휠체어')!=-1){
+          }else if (purifiedQuestion.indexOf('휠체어')!=-1){
             console.log('휠체어는 간호사실 옆에 배치되어 있습니다.');
-          }else if (question.replace(/ /gi, '').indexOf('정수기')!=-1){
+          }else if (purifiedQuestion.indexOf('정수기')!=-1){
             console.log('정수기는 각 병동 휴게실에 배치되어 있습니다.');
-          }else if (question.replace(/ /gi, '').indexOf('MRI검사실')!=-1){
+          }else if (purifiedQuestion.indexOf('MRI검사실')!=-1){
             console.log('본관 1층 응급실 맞은편에 있습니다.');
           }else{
             console.log('어디를 말씀하는지 모르겠어요.. 다시한번 말씀해주세요!');
